e420fa7119e7bdf32105feaad6ca4e9969118afe
samwanyua <samexample8@gmail.com>
2026-02-13 00:00:40 +0300

    Update auth

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,6 @@
 // src/utils/api.ts
 
-const API_BASE_URL = "https://sauticare-backend-6rg6.onrender.com";
+const API_BASE_URL = "https://sauticare-backend-6rg6.onrender.com/api/v1";
 
 /* -------------------------------- AUTH -------------------------------- */
 
